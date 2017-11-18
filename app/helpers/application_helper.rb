a433914ddf5c9c507a9c81388053f4eec6184625
Quentin Decock <quentin.decock@gmail.com>
2017-11-18 19:54:32 +0100

    fixes issue with conditional header highlight and locale

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def current_page_is_homepage?
+    is_a_match = false
+    ['/','/en','/fr','/en/pages/homepage','/fr/pages/homepage'].each do |path|
+      is_a_match = is_a_match || current_page?(path)
+    end
+    is_a_match
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'high_voltage/pages#show', :id => 'homepage', :format => false
+  root 'high_voltage/pages#show', :id => 'homepage', :locale => I18n.default_locale, :format => false
   get '/:locale' => 'high_voltage/pages#show', :id => 'homepage'
   scope "/:locale", locale: /en|fr/ do
       get "/pages/:id" => "high_voltage/pages#show", :as => :page, :format => false
