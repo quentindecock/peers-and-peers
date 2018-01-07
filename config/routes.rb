656f3fc7f2c8b72697ff86f05fc20e1d3c5609e0
Quentin Decock <quentin.decock@gmail.com>
2018-01-07 19:01:17 +0100

    optional :locale parameters in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'high_voltage/pages#show', :id => 'homepage', :locale => I18n.default_locale, :format => false
   get '/:locale' => 'high_voltage/pages#show', :id => 'homepage'
-  scope "/:locale", locale: /en|fr/ do
-      get "/pages/:id" => "high_voltage/pages#show", :as => :page, :format => false
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/, defaults: {locale: "en"} do
+    get "/pages/:id" => "high_voltage/pages#show", :as => :page, :format => false
   end
 end
