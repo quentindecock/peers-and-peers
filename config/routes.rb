ecd41ca7d5303cad68218bebcec39528d3081870
Quentin Decock <quentin.decock@gmail.com>
2018-02-12 16:34:27 +0100

    fixes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   root 'high_voltage/pages#show', :id => 'homepage', :locale => I18n.default_locale, :format => false
-  get '/:locale' => 'high_voltage/pages#show', :id => 'homepage'
+
+  # redirections - to remove when Google took it into account
+  get 'index.html',              to: redirect('/',301)
+  get 'clients.html',            to: redirect('/en/pages/credentials',301)
+  get 'team.html',               to: redirect('/en/pages/team',301)
+  get 'contact.html',            to: redirect('/en/pages/contact',301)
+  get 'consulting.html',         to: redirect('/en/pages/consulting',301)
+  get '/en/pages/due-diligence', to: redirect('/en/pages/due-diligences',301)
+
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/, defaults: {locale: "en"} do
+    get '/:locale' => 'high_voltage/pages#show', :id => 'homepage'
     get "/pages/:id" => "high_voltage/pages#show", :as => :page, :format => false
   end
 end
